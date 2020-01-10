665682c080d1b6ed772e97d0a07e620f7f41e6cd
Cassidy Fortner <cassidy.fortner@gmail.com>
2020-01-09 22:01:39 -0600

    put a comment in getmanager.js to restructure project, will come back

---
+++ app.js
@@ -0,0 +1,9 @@
+const manage = require("./lib/getManager");
+
+
+let manager =  manage.getManager();
+
+console.log(manager);
+
+//look into just making an overarching buildTeam() function that just calls all functions together and builds everything here.
+
