3b14810c9215ccfddbf1f9a1aa50a9cc510f9a62
Cassidy Fortner <cassidy.fortner@gmail.com>
2020-01-13 13:54:03 -0600

    finished app, correctly generating html into the output folder
    Just need to add everything for readme, app works, correctly generates an html file into the output folder

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const team = require("./lib/buildTeam");
 
-
 team.buildTeam();
 
 //look into just making an overarching buildTeam() function that just calls all functions together and builds everything here.
